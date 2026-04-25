8f7bac392666636c77119e1d268429ce2e85adfb
shldlqj2 <87292495+shldlqj2@users.noreply.github.com>
2026-04-25 16:05:38 +0900

    Title: 돌 잘 치우기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260425/돌 잘 치우기/clear-stones-well.py
+++ 260425/돌 잘 치우기/clear-stones-well.py
@@ -13,65 +13,57 @@ for _ in range(k):
 rcs=list(zip(r,c))
 
 from collections import deque
+from itertools import combinations
 
 
 dr=[1,-1,0,0]
 dc=[0,0,1,-1]
 
-visited=[[False]* n for _ in range(n)]
 
 cnt=0
 
+rocks=[]
 
-#일단 기본적으로 탐사 가능 한 곳 전부 확인
-
-for row,col in rcs:
-    queue=deque()
-    queue.append((row,col))
-    if not visited[row][col]:
-        
-        visited[row][col]=True
-        cnt+=1
-
-    while queue:
-        cr,cc=queue.popleft()
-        for i in range(4):
-            nr,nc=cr+dr[i],cc+dc[i]
-            if 0<=nr<n and 0<=nc<n and grid[nr][nc]==0 and not visited[nr][nc]:
-                visited[nr][nc]=True
-                queue.append((nr,nc))
-                cnt+=1
-                
-
-tempcnt=0
 for row in range(n):
     for col in range(n):
-        if grid[row][col]==0 and visited[row][col]:
-            queue=deque()
-            queue.append((row,col,0,0))#r,c,wallmove,currcnt
-            
-            newvisited=[row[:] for row in visited]
+        if grid[row][col]==1:
+            rocks.append((row,col))
+
+removelist=list(combinations(rocks,m))
 
 
-            while queue:
-                cr,cc,wallmove,currcnt=queue.popleft()
-                for i in range(4):
-                    nr,nc=cr+dr[i],cc+dc[i]
+for list1 in removelist:
+    tempgrid=[row[:] for row in grid]
+    for row,col in list1:
+        tempgrid[row][col]=0
+    
+    currcnt=0
 
-                    if 0<=nr<n and 0<=nc<n and not newvisited[nr][nc]:
-                        if grid[nr][nc]==0:
-                            queue.append((nr,nc,wallmove,currcnt+1))
-                            newvisited[nr][nc]=True
-                            
-                        if grid[nr][nc]==1:
-                            if wallmove<m:
-                                queue.append((nr,nc,wallmove+1,currcnt))
-                                newvisited[nr][nc]=True
-                tempcnt=max(tempcnt,currcnt)
+    visited=[[False]*n for _ in range(n)]
 
-            
+    for row,col in rcs:
+        queue=deque()
+        queue.append((row,col))
 
-print(cnt+tempcnt+m)
+        
+
+        if not visited[row][col]:
+            visited[row][col]=True
+            currcnt+=1
+
+
+        while queue:
+            cr,cc=queue.popleft()
+            for i in range(4):
+                nr,nc=cr+dr[i],cc+dc[i]
+                if 0<=nr<n and 0<=nc<n and tempgrid[nr][nc]==0 and not visited[nr][nc]:
+                    visited[nr][nc]=True
+                    queue.append((nr,nc))
+                    currcnt+=1
+
+    cnt=max(cnt,currcnt)
+
+print(cnt)
                             
 
 
