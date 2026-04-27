62889ea021664f998fc5053af19bf209d8e4fe26
shldlqj2 <87292495+shldlqj2@users.noreply.github.com>
2026-04-27 12:56:36 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260427/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 260427/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -3,7 +3,7 @@ N = int(input())
 # Please write your code here.
 from collections import deque
 
-arr=[0]*(N+1)
+arr=[0]*(1000001)
 
 
 queue=deque()
@@ -13,24 +13,25 @@ while queue:
     currnum=queue.popleft()
     currtime=arr[currnum]
 
+    cnd3=currnum//3
+    cnd2=currnum//2
 
     if currnum==1:
+        print(currtime)
         break
 
-    if 1<=currnum-1<N and arr[currnum-1]==0:
+    if 1<=currnum-1<1000001 and arr[currnum-1]==0:
         arr[currnum-1]=currtime+1
         queue.append(currnum-1)
     
-    if 1<=currnum+1<N and arr[currnum+1]==0:
+    if 1<=currnum+1<1000001 and arr[currnum+1]==0:
         arr[currnum+1]=currtime+1
         queue.append(currnum+1)
     
-    if currnum%2==0 and 1<=currnum//2<N and arr[currnum//2]==0:
-        arr[currnum//2]=currtime+1
-        queue.append(currnum//2)
+    if currnum%2==0 and 1<=cnd2<1000001 and arr[cnd2]==0:
+        arr[cnd2]=currtime+1
+        queue.append(cnd2)
 
-    if currnum%3==0 and 1<=currnum//3<N  and arr[currnum//3]==0:
-        arr[currnum//3]=currtime+1
-        queue.append(currnum//3)
-
-print(arr[1])
+    if currnum%3==0 and 1<=cnd3<1000001  and arr[cnd3]==0:
+        arr[cnd3]=currtime+1
+        queue.append(cnd3)
