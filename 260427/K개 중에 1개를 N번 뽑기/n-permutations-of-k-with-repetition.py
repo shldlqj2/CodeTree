95cbd78ec9a27eb749b8d931fc4e3bdd2a006cf1
shldlqj2 <87292495+shldlqj2@users.noreply.github.com>
2026-04-27 15:41:19 +0900

    Title: K개 중에 1개를 N번 뽑기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260427/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.py
+++ 260427/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.py
@@ -1,6 +1,26 @@
 K, N = map(int, input().split())
 
 # Please write your code here.
+
+subarr=[]
+
+def print_permutation():
+    for num in subarr:
+        print(num, end=" ")
+    print()
+
+def find_per(cnt):
+    if cnt==N:
+        print_permutation()
+        return
+    for i in range(1,K+1):
+        subarr.append(i)
+        find_per(cnt+1)
+        subarr.pop()
+find_per(0)
+
+
+"""
 from itertools import product
 
 arr=[i for i in range(1,K+1)]
@@ -11,3 +31,4 @@ arr=list(product(arr,repeat=N))
 
 for subarr in arr:
     print(*subarr)
+    """
