b8382927c7c22e6eddd0a947389d23eb01892a16
shldlqj2 <87292495+shldlqj2@users.noreply.github.com>
2026-04-28 17:24:49 +0900

    Title: 사다리 타기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260428/사다리 타기/ladder-game.py
+++ 260428/사다리 타기/ladder-game.py
@@ -18,36 +18,47 @@ def routingStart(edgemap):
         result.append(curr)
     return result
 
-def findMinimum(linecnt):
+def findMinimum(idx,linecnt):
+    global result
+
+    if result<=linecnt:
+        return
 
 
     currresult=routingStart(curredges)
     cnt=0
+    
     for i in range(len(currresult)):
         if currresult[i]!=defaultResult[i]:
             break
         else:
             cnt+=1
     if cnt == n:
-        global result
         result=min(result,linecnt)
-        return        
-
-    if linecnt==m:
         return
     
+    if idx==m:
+        return
     
 
-    for i in range(linecnt,m):
-        for j in range(1,n):
-            curredges.append((j,i+1))
-            findMinimum(linecnt+1)
-            curredges.pop()
+    curredges.append(edges[idx])
+    findMinimum(idx+1,linecnt+1)
+    curredges.pop()
+
+    findMinimum(idx+1,linecnt)
+
+    # for i in range(linecnt+1):
+    #     for j in range(1,n):
+    #         if (j,i+1) in curredges or (j-1,i+1) in curredges or (j+1,i+1) in curredges:
+    #             continue
+    #         curredges.append((j,i+1))
+    #         findMinimum(linecnt+1)
+    #         curredges.pop()
         
 
 defaultResult=routingStart(edges)
 
 result=float('inf')
 
-findMinimum(0)
+findMinimum(0,0)
 print(result)
