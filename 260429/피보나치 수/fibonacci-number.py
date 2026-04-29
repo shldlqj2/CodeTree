5e471606c435e54a66e6c9325cd4bdbc926fc621
shldlqj2 <87292495+shldlqj2@users.noreply.github.com>
2026-04-29 17:25:15 +0900

    Title: 피보나치 수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260429/피보나치 수/fibonacci-number.py
+++ 260429/피보나치 수/fibonacci-number.py
@@ -2,11 +2,11 @@ N = int(input())
 
 # Please write your code here.
 dp=[1]*(N+1)
-dp.append(1)
-dp.append(1)
 
 def fibo(n):
     if n==1 or n==2:
+        return dp[n]
+    if dp[n]!=1:
         return dp[n]
     else:
         dp[n]=fibo(n-1)+fibo(n-2)
