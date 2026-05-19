2d29c081a188303d56edf105f615be0e5db29ecf
shldlqj2 <87292495+shldlqj2@users.noreply.github.com>
2026-05-19 12:43:02 +0900

    [Passed] 최대 증가 부분 수열 (65ms, 17MB)

---
+++ 260519/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -0,0 +1,14 @@
+n = int(input())
+m = list(map(int, input().split()))
+
+# Please write your code here.
+dp=[0]*n
+maxlen=0
+for i in range(n):
+    maxdp=0
+    for j in range(i):
+        if m[j]<m[i]:
+            maxdp=max(dp[j],maxdp)
+    dp[i]=maxdp+1
+    maxlen=max(dp[i],maxlen)
+print(maxlen)
