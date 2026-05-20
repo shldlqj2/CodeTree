4e65330d33d49053584ec392ce6148a0a08b565d
shldlqj2 <87292495+shldlqj2@users.noreply.github.com>
2026-05-20 16:20:55 +0900

    [Passed] 부분 수열의 합이 M (102ms, 18MB)

---
+++ 260520/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+
+# Please write your code here.
+dp=[float('inf')]*(m+1)
+dp[0]=0
+
+for i in range(n):
+    for j in range(m,-1,-1):
+        if j>=A[i]:
+            if dp[j-A[i]]==float('inf'):
+                continue
+            dp[j]=min(dp[j],dp[j-A[i]]+1)
+if dp[m]==float('inf'):
+    print(-1)
+else:
+    print(dp[m])
