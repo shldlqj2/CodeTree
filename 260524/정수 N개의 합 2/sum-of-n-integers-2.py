17ccf8b386a7f7e92f8ecabc4641dcb0e0f07a77
shldlqj2 <87292495+shldlqj2@users.noreply.github.com>
2026-05-24 13:09:10 +0900

    [Passed] 정수 N개의 합 2 (81ms, 27MB)

--- 260524/정수 N개의 합 2/sum-of-n-integers-2.py
+++ 260524/정수 N개의 합 2/sum-of-n-integers-2.py
@@ -3,7 +3,7 @@ arr = list(map(int, input().split()))
 
 # Please write your code here.
 s=[0]*(n)
-for i in range(1,n):
+for i in range(1,n): #O(N)
     s[i]=s[i-1]+arr[i]
-sk=[s[j]-s[j-k] for j in range(k,len(s)) ]
+sk=[s[j]-s[j-k] for j in range(k,len(s)) ] #O(N-k)-> O(1)
 print(max(sk))
